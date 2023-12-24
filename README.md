3d4b9fa36dc4f2be1fdcfd8760ef16f126906de1
BARTHELEMY Stephane <stephane@sbarthelemy.com>
2023-12-24 04:59:36 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# CONTROL VIDEO / SERIAL on WEB 
+
+Control the video input & the serial over a web interface 
+
+
+## Getting Started
+
+### Dependencies
+
+see pre_install.txt for dependency on a linux (debian /ubuntu)
+
+### Installing
+
+
+
+### Executing program
+
+node index.js 
+
+## Authors
+
+BARTHELEMY Stephane 
+
+## Version History
+
+* 0.1
+    * Initial Release
+
+## License
+
+This project is licensed under the ISC License - see the LICENSE file for details
+
+## Acknowledgments
---
+++ index.js
@@ -0,0 +1,194 @@
+/* 
+Copyright (C) 2023 BARTHELEMY Stephane
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+const express = require('express'); 
+const path = require('path'); 
+const app = express(); 
+const server = require('http').Server(app);
+const io = require('socket.io')(server); 
+const cv = require('@u4/opencv4nodejs');
+const fs = require('fs');
+const os = require('os');
+const sp = require("serialport");
+const port = 3000;
+const FPS = 25
+var counter = 0 ; 
+
+var cap = null ; 
+var cap2 = null ; 
+
+var SPort = sp.SerialPort;
+
+const serialPort = new SPort({
+    path: "/dev/ttyUSB0",
+    baudRate: 115200,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowcontrol: false,
+    buffersize: 20480
+  });
+
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'/static/index.html')); 
+}); 
+
+app.use('/static', express.static(path.join(__dirname, '/static')))
+app.use('/lib', express.static(path.join(__dirname, '/lib')))
+
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    console.log('chek interface video ');
+
+    const interfaces = [];
+    for (let i = 0; i < 10 ; i++) {
+        try {
+            var camera = new cv.VideoCapture(i);
+            if (!!camera) {
+                interfaces.push(i);
+            } 
+            camera.release();
+        } catch (e){
+            console.log("Couldn't start camera:", e)
+        }    
+    }
+
+    console.log(interfaces); 
+    io.emit('select1_list',interfaces); 
+    io.emit('select2_list',interfaces); 
+    
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('select1_change', function (data) {
+        try {
+            cap.release();
+        } catch (e){
+            console.log("Couldn't stop stream 1:", e)
+        }              
+        if (data >= 0) {
+            try {
+                cap = new cv.VideoCapture(parseInt(data,10));
+                cap.set(cv.CAP_PROP_FRAME_WIDTH, 640);
+                cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480);
+            } catch (e){
+                console.log("Couldn't start stream 1:", e)
+            }    
+        } else
+        {
+            cap = null ; 
+        }
+    });
+    
+    socket.on('select2_change', function (data) {
+        try {
+            cap2.release();
+            delete cap2 ; 
+        } catch (e){
+            console.log("Couldn't stop stream 1:", e)
+        }              
+        if (data >= 0) {
+            try {
+                cap2 = new cv.VideoCapture(parseInt(data,10));
+                cap2.set(cv.CAP_PROP_FRAME_WIDTH, 640);
+                cap2.set(cv.CAP_PROP_FRAME_HEIGHT, 480);
+    
+            } catch (e){
+                console.log("Couldn't start stream 2:", e)
+            }    
+        } else 
+        {
+            cap2 = null
+        }
+    });
+    
+    console.log("init serial ")
+
+    serialPort.on('data', function (data_stream) {
+        const buf = Buffer.from(data_stream);
+        const data = buf.toString('ascii')
+        socket.emit ('data',data);         
+    });
+    
+
+    socket.on('serial_char', function (data) {
+    //    console.log('caractere from terminal : ' , data , typeof (data)) ; 
+        serialPort.write(data);
+    })
+
+    console.log("Set video ")
+
+     setInterval(() => {
+        if (!!cap) {
+            try {
+                const frame = cap.read(); 
+                const imagElm = cv.imencode('.jpg', frame).toString('base64') ; 
+                io.emit('image1',imagElm); 
+            } catch (e){
+                console.log("Couldn't get stream 2:", e)
+            }
+        }
+                  
+    },1000 / FPS);
+    setInterval(() => {
+        if (!!cap2) {
+            try {           
+                const frame = cap2.read(); 
+                
+                const imagElm = cv.imencode('.jpg', frame).toString('base64') ; 
+                io.emit('image2',imagElm); 
+            } catch (e){
+                console.log("Couldn't get stream 2:", e)
+            }         
+        }    
+    },1000 / FPS);
+
+
+    setInterval(()=> {
+        const cpus = os.cpus();
+        var o = {}
+        var key='CPU Info'
+        
+        o[key] = []
+        var data = {
+            machinetype: os.machine(),
+            platform: os.platform(),
+            type: os.type(),
+            cpumodel : cpuModel = os.cpus()[0].model,
+            version : os.version(),
+            hostname: os.hostname(),
+            uptime: os.uptime()
+        } 
+        o[key].push(data) ; 
+        const json = JSON.stringify(o);
+        //console.log(json) ; 
+        socket.emit('info',json) ; 
+    },1000)
+
+
+  })
+
+serialPort.on("open", function () {
+    console.log('serialPort open');
+});
+
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
